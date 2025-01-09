7f6c3d02b48992af561ac69878a37346ae274c29
aidbar <47143229+aidbar@users.noreply.github.com>
2025-01-09 18:35:34 +0200

    Table to display a user's workspaces in dashboard implemented

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../components/ui/button"
 import { useNavigate } from "react-router-dom"
 import { Header } from "../components/Header" // Import the Header component
+import { WorkspaceTable } from "../components/WorkspacesTable"
 
 export function Dashboard() {
   const navigate = useNavigate()
@@ -10,7 +11,8 @@ export function Dashboard() {
         <Header />
         <div className="flex flex-col gap-10 h-screen p-[0.5rem]">
             <h1 className="text-2xl">Welcome, USER!</h1>
-            <Button className="w-24" onClick={() => navigate("/")}>Go to Home</Button>
+            <Button className="w-28" onClick={() => navigate("/")}>New workspace</Button>
+            <WorkspaceTable />
         </div>
     </div>
   )
