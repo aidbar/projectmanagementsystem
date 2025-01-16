874406f8ada32d3450ac5a4e1350395cd3d0e003
aidbar <47143229+aidbar@users.noreply.github.com>
2025-01-16 13:44:12 +0200

    Protected routes implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
-
 import App from "./App"
 import "./index.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
@@ -11,14 +10,19 @@ import { ProjectBoard } from "./pages/ProjectBoard"
 
 const queryClient = new QueryClient()
 
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  const isLoggedIn = localStorage.getItem("accessToken") ? true : false
+  return isLoggedIn ? element : <h1>Access Denied</h1>;
+};
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="workspace/:id" element={<Workspace />} />
-        <Route path="project-board/:id" element={<ProjectBoard />} />
+        <Route path="dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+        <Route path="workspace/:id" element={<ProtectedRoute element={<Workspace />} />} />
+        <Route path="project-board/:id" element={<ProtectedRoute element={<ProjectBoard />} />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
