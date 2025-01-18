1ac4307f55ffee7d8bad0ba46cb9e0142166ea26
aidbar <47143229+aidbar@users.noreply.github.com>
2025-01-18 14:54:51 +0200

    Fetch error handling in workspace page

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -4,18 +4,30 @@ import { Header } from "../components/Header"
 import { ProjectBoardsTable } from "../components/ProjectBoardsTable"
 import { useEffect, useState } from "react"
 import api from "../api"
+import { AxiosError } from "axios"
 
 export function Workspace() {
   const navigate = useNavigate()
   const { id } = useParams<{ id: string }>()
   const [workspaceData, setWorkspaceData] = useState({ name: "", description: "", isPublic: false, createdAt: "", updatedAt: "", creatorUsername: "" })
+  const [fetchError, setFetchError] = useState("")
 
   useEffect(() => {
     async function fetchWorkspaceData() {
+      setFetchError("")
       try {
         const response = await api.get(`/v1/Workspaces/${id}`)
         setWorkspaceData(response.data)
       } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.data) {
+            setFetchError(error.response.data)
+          } else {
+            setFetchError("Error fetching workspace data")
+          }
+        } else {
+          setFetchError("An error occurred")
+        }
         console.error("Failed to fetch workspace data:", error)
       }
     }
@@ -27,12 +39,13 @@ export function Workspace() {
     <div>
       <Header />
       <div className="flex flex-col gap-10 h-screen p-[0.5rem]">
+        {fetchError && <p className="text-red-500">{fetchError}</p>}
         {workspaceData ? (
           <div>
             <h1 className="text-2xl text-center p-4">{workspaceData.name}</h1>
             <p className="italic text-center">{workspaceData.description}</p>
             <div>
-            <p>Created by: {workspaceData.creatorUsername}</p>
+            <p>Created by: {workspaceData.creatorUsername || <em>username hidden from others</em>}</p>
             <p>Created at: {new Date(workspaceData.createdAt).toLocaleString()}</p>
             <p>Last updated: {new Date(workspaceData.updatedAt).toLocaleString()}</p>
             <p>Visibility: <strong>{workspaceData.isPublic ? "Public" : "Private"}</strong></p>
