d4bedc88b713f7f576a6972e80753377e0c2ef3b
aidbar <47143229+aidbar@users.noreply.github.com>
2025-01-28 16:58:15 +0200

    Project board editing in `ProjectBoard.tsx`

--- src/pages/ProjectBoard.tsx
+++ src/pages/ProjectBoard.tsx
@@ -10,6 +10,9 @@ import { ColumnsProvider } from "@/context/ColumnsContext"
 import { PrioritiesProvider } from "@/context/PrioritiesContext"
 import Tasks from "@/api/tasks"
 import { TasksProvider } from "@/context/TasksContext"
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/dropdown";
+import { Edit } from "lucide-react";
 
 export function ProjectBoard() {
   const navigate = useNavigate()
@@ -17,6 +20,85 @@ export function ProjectBoard() {
   const [projectBoardData, setProjectBoardData] = useState({ name: "", description: "", isPublic: false, createdAt: "", updatedAt: "", creatorUsername: "" })
   const [fetchError, setFetchError] = useState("")
   const [isPopupOpen, setIsPopupOpen] = useState(false)
+  const [isEditing, setIsEditing] = useState({
+    name: false,
+    description: false,
+    isPublic: false,
+  });
+
+  const [projectBoardDetails, setProjectBoardDetails] = useState({
+    name: projectBoardData.name,
+    description: projectBoardData.description,
+    isPublic: projectBoardData.isPublic,
+  });
+
+  const [toastOpen, setToastOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [hasError, setHasError] = useState(false);
+
+  const handleEditToggle = (field: keyof typeof isEditing): void => {
+    setIsEditing((prev) => ({ ...prev, [field]: !prev[field] }));
+  };
+
+  const handleChange = (e: { target: { name: any; value: any } }): void => {
+    const { name, value } = e.target;
+    setProjectBoardDetails((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSave = async (field: keyof typeof isEditing) => {
+    setHasError(false);
+
+    try {
+      if (projectBoardDetails.name.length === 0) {
+        setHasError(true);
+        setToastMessage("Project board name is required.");
+        setToastOpen(true);
+        return;
+      } else if (projectBoardDetails.name.length > 200) {
+        setHasError(true);
+        setToastMessage("Project board name must not exceed 200 characters.");
+        setToastOpen(true);
+        return;
+      }
+
+      await api.put(`/ProjectBoards/${id}`, {
+        ...projectBoardDetails,
+      }).then((response) => {
+        const data = response.data.data;
+        console.log("Changes saved:", data);
+        setProjectBoardData((prev) => ({
+          ...prev,
+          name: data.name,
+          description: data.description,
+          isPublic: data.isPublic,
+          createdAt: data.createdAt,
+          updatedAt: data.updatedAt,
+          //creatorUsername: data.creatorUsername || prev.creatorUsername,
+        }));
+        setProjectBoardDetails((prev) => ({
+          ...prev,
+          name: data.name,
+          description: data.description,
+          isPublic: data.isPublic,
+        }));
+        setIsEditing((prev) => ({ ...prev, [field]: false }));
+        setToastMessage("Changes saved");
+        setToastOpen(true);
+      });
+    } catch (error) {
+      console.error("Error saving changes:", error);
+      if (error instanceof AxiosError) {
+        if (error.response?.data) {
+          setToastMessage(error.response.data);
+        } else {
+          setToastMessage("Failed to save changes");
+        }
+      } else {
+        setToastMessage("An error occurred");
+      }
+      setToastOpen(true);
+    }
+  };
 
   useEffect(() => {
     async function fetchProjectBoardData() {
@@ -41,6 +123,10 @@ export function ProjectBoard() {
     fetchProjectBoardData()
   }, [id])
 
+  useEffect(() => {
+    setProjectBoardDetails(projectBoardData);
+  }, [projectBoardData]);
+
   return (
     <div>
       <Header />
@@ -48,12 +134,70 @@ export function ProjectBoard() {
         {fetchError && <p className="text-red-500">{fetchError}</p>}
         {projectBoardData ? (
           <div>
-            <h1 className="text-2xl text-center p-4">{projectBoardData.name}</h1>
-            <p className="italic text-center">{projectBoardData.description}</p>
+            <h1 className="text-2xl text-center p-4">
+              {isEditing.name ? (
+                <div className="flex justify-center items-center">
+                  <Input
+                    type="text"
+                    name="name"
+                    value={projectBoardDetails.name}
+                    onChange={handleChange}
+                    onBlur={() => handleSave('name')}
+                    className={`border border-gray-500 p-3 w-50 flex justify-center items-center ${hasError ? 'border-red-500' : ''}`}
+                  />
+                </div>
+              ) : (
+                <div className="flex justify-center items-center">
+                  {projectBoardDetails.name} <Button variant={"ghost"} onClick={() => handleEditToggle('name')}><Edit /></Button>
+                </div>
+              )}
+            </h1>
+            <p className="italic text-center">
+              {isEditing.description ? (
+                <div className="flex justify-center items-center">
+                  <Input
+                    name="description"
+                    value={projectBoardDetails.description}
+                    onChange={handleChange}
+                    onBlur={() => handleSave('description')}
+                    className="border border-gray-500 p-3 w-50 flex justify-center items-center"
+                  />
+                </div>
+              ) : (
+                <>
+                  {projectBoardDetails.description} <Button variant={"ghost"} onClick={() => handleEditToggle('description')}><Edit /></Button>
+                </>
+              )}
+            </p>
             <div>
               <p>Created at: {new Date(projectBoardData.createdAt).toLocaleString()}</p>
               <p>Last updated: {new Date(projectBoardData.updatedAt).toLocaleString()}</p>
-              <p>Visibility: <strong>{projectBoardData.isPublic ? "Public" : "Private"}</strong></p>
+              <p>
+                Visibility: <strong>{isEditing.isPublic ? (
+                  <Select
+                    value={projectBoardDetails.isPublic ? "true" : "false"}
+                    onValueChange={(value) => {
+                      if (value == "true") {
+                        setProjectBoardDetails((prev) => ({ ...prev, isPublic: true }));
+                      } else {
+                        setProjectBoardDetails((prev) => ({ ...prev, isPublic: false }));
+                      }
+                    }}
+                  >
+                    <SelectTrigger className="border border-gray-500 p-3 w-50 flex justify-center items-center" onBlur={() => handleSave('isPublic')}>
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="true">Public</SelectItem>
+                      <SelectItem value="false">Private</SelectItem>
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <>
+                    {projectBoardDetails.isPublic ? 'Public' : 'Private'} <Button variant={"ghost"} onClick={() => handleEditToggle('isPublic')}><Edit /></Button>
+                  </>
+                )}</strong>
+              </p>
             </div>
             <p>Project Board ID: {id}</p>
           </div>
