2c428d6fd277ca43c1ccc9aac38e7e9da5065a36
aidbar <47143229+aidbar@users.noreply.github.com>
2025-01-31 15:45:06 +0200

    Token refresh mechanism in API interceptor implemented

--- src/api/index.ts
+++ src/api/index.ts
@@ -24,15 +24,66 @@ const api = axios.create({
   withCredentials: true
 })
 
-// use this to handle errors gracefully
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response.status === 500) {
-//       throw new Error(error.response.data)
-//     }
-//   }
-// )
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        }).then(token => {
+          originalRequest.headers['Authorization'] = 'Bearer ' + token;
+          return axios(originalRequest);
+        }).catch(err => {
+          return Promise.reject(err);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = localStorage.getItem('refreshToken');
+      const accessToken = localStorage.getItem('accessToken');
+      return new Promise(function (resolve, reject) {
+        axios.post('/Authenticate/refresh', { refreshToken: refreshToken, accessToken: accessToken })
+          .then(({ data }) => {
+            localStorage.setItem('accessToken', data.accessToken);
+            localStorage.setItem('refreshToken', data.refreshToken);
+            api.defaults.headers.common['Authorization'] = 'Bearer ' + data.accessToken;
+            originalRequest.headers['Authorization'] = 'Bearer ' + data.accessToken;
+            processQueue(null, data.accessToken);
+            resolve(axios(originalRequest));
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            reject(err);
+          })
+          .finally(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.request.use((config) => {
   const isPublic = PUBLIC_ROUTES.includes(config.url || "")
